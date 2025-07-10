c55c065f4dd33acacd6be0a880e16ebabf98d093
Jkerdley <jk-erley@yandex.ru>
2025-07-10 23:53:05 +0700

    feat: adds radixui and some architecture improvements

---
+++ src/core/store/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./api";
+import someSlice from "./someSlice";
+
+export const store = configureStore({
+    reducer: {
+        [api.reducerPath]: api.reducer,
+        someData: someSlice,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+    devTools: true,
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ src/modules/translation/types.ts
@@ -0,0 +1,3 @@
+export type TranslationType = {
+  language: string;
+};
