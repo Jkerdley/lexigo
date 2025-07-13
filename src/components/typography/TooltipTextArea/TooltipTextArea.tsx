d10248a2235887eb0fef1c0922f29d84b2d20e67
Jkerdley <jk-erley@yandex.ru>
2025-07-14 02:40:28 +0700

    fix: fix router bugs

--- src/components/typography/TooltipTextArea/TooltipTextArea.tsx
+++ src/components/typography/TooltipTextArea/TooltipTextArea.tsx
@@ -1,10 +1,14 @@
 import "./tooltip-text-area.scss";
-import {useCurrentTranslation} from "../../../modules/translation/hooks/useCurrentTranslation.ts";
+import { useCurrentTranslation } from "../../../modules/translation/hooks/useCurrentTranslation.ts";
+import { Outlet } from "react-router-dom";
 
 export const TooltipTextArea = () => {
     const tr = useCurrentTranslation();
-    return <article className="tooltip-text-area">{tr?.translated} <Outlet /></article>;
-
+    return (
+        <article className="tooltip-text-area">
+            {tr?.translated} <Outlet />
+        </article>
+    );
 };
 
 export default TooltipTextArea;
